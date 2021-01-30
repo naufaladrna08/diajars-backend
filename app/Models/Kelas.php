e7aa613c77a08246563d51820fe3e7e480ec7703
Naufal Adriansyah <naufaladrn90@gmail.com>
2021-01-31 01:34:50 +0700

    ditambahkan guruId pada tabel kelas

--- app/Models/Kelas.php
+++ app/Models/Kelas.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Kelas extends Model {
   use HasFactory;
 
-  protected $fillable = ['namaKelas', 'kodeKelas',	'jenisKelas'];
+  protected $fillable = ['guruId', 'namaKelas', 'kodeKelas',	'jenisKelas'];
 }
--- database/migrations/2021_01_27_174124_create_kelas_table.php
+++ database/migrations/2021_01_27_174124_create_kelas_table.php
@@ -15,7 +15,7 @@ class CreateKelasTable extends Migration
     {
         Schema::create('kelas', function (Blueprint $table) {
             $table->id();
-            $table->integer('guruId');
+            $table->integer('guruId')->nullable();
             $table->text("namaKelas");
             $table->char("kodeKelas");
             $table->char("jenisKelas");
