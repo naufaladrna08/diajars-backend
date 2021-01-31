343f89ba8441c8cef282e6dc85c8da4044ac892c
Naufal Adriansyah <naufaladrn90@gmail.com>
2021-02-01 06:22:01 +0700

    ditambahkan get_materi dan get_games

--- app/Http/Controllers/KelasController.php
+++ app/Http/Controllers/KelasController.php
@@ -8,11 +8,25 @@ use App\Models\User;
 use App\Models\RelasiKelas;
 use App\Models\TableSosialMedia;
 use App\Models\Kelas;
+use App\Models\MateriTable;
+use App\Models\GameTable;
 
 class KelasController extends Controller {
   public function check_class_state(Request $r) {
   	$kelas = Kelas::where('guruId', $r['guruId'])->first();
 
-  	return $kelas['statusPaket'];
+  	return response()->json($kelas, 200);
+  }
+
+  public function get_materi(Request $r) {
+  	$materi = MateriTable::where('jenisKelas', $r['jenisKelas'])->get();
+
+  	return response()->json($materi, 200);
+  }
+
+  public function get_games(Request $r) {
+  	$games = GameTable::where('jenisKelas', $r['jenisKelas'])->get();
+
+  	return response()->json($games, 200);
   }
 }
