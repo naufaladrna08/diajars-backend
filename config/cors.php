2fd515cf5525f82c4e385a57db56a45bff251821
Naufal Adriansyah <naufaladrn90@gmail.com>
2021-02-20 13:44:28 +0700

    cors problem

--- config/cors.php
+++ config/cors.php
@@ -18,17 +18,11 @@ return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['http://localhost:3000'],
-
+    'allowed_origins' => env('ALLOWED_ORIGINS'),
     'allowed_origins_patterns' => [],
-
     'allowed_headers' => ['*'],
-
     'exposed_headers' => [],
-
     'max_age' => 0,
-
     'supports_credentials' => false,
 
 ];
