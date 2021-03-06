7eba5bf369219bdfa7a5b3e220a16d8d6458c64a
Naufal Adriansyah <naufaladrn90@gmail.com>
2021-03-06 22:35:49 +0700

    fixed cors problem

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,11 @@ use App\Http\Controllers\NilaiController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+header('Access-Control-Allow-Origin:  *');
+header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
+
 Route::middleware('guest')->group(function () {
 	# User
 	Route::post('login', [UserController::class, 'login']);
